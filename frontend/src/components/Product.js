ce9eb3d00f374efdbcdcba85a832ab239efdde6b
hemel18681 <hemel18103112@gmail.com>
2020-12-27 22:48:36 +0600

    indivisual item seen done.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
-import Rating from '../Rating'
+import React from 'react'   
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
 const Product = ({product}) => {
     return (
+
         <Card className='my-3 p-3 rounded'>
-            <a herf={`/product/${product._id}`}>
-                <Card.Img src = {product.image} variant='top'/>
-            </a>
+            <Link to={`/product/${product._id}`}>
+                <Card.Img src={product.image} variant='top'/>
+            </Link>
             <Card.Body>
-                <a herf={`/product/${product._id}`}>
-                    <Card.Title as = 'div'><strong>{product.name}</strong></Card.Title>
-                </a>
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
+                </Link>
                 <Card.Text as='div'>
-                    <Rating value={product.rating} text={`${product.numRevies} reviews`}/>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
                 </Card.Text>
               <Card.Text as='h3'>{product.price}tk</Card.Text>  
             </Card.Body>
@@ -21,3 +23,4 @@ const Product = ({product}) => {
 }
 
 export default Product
+    
